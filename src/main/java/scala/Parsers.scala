29c8996c2964f7061f8c74d9d5dc07d6b6e178e8
cyz12 <1093491992@qq.com>
2020-08-17 23:02:48 +0800

    Add DML

--- src/main/java/scala/Parsers.scala
+++ src/main/java/scala/Parsers.scala
@@ -8,11 +8,13 @@ class SimpleParser extends RegexParsers {
   def word: Parser[String]   = """[a-z]+""".r       ^^ { _.toString }
   def number: Parser[Int]    = """(0|[1-9]\d*)""".r ^^ { _.toInt }
   def freq: Parser[WordFreq] = word ~ number        ^^ { case wd ~ fr => WordFreq(wd,fr) }
+  //
+  def keyworld_DML: Parser[String] = "(?i)(SELECT|UPDATE|DELETE|INSERT)".r ^^{_.toString}
 }
 
 object TestSimpleParser extends SimpleParser {
   def main(args: Array[String]) = {
-    parse(freq, "johnny 121") match {
+    parse(keyworld_DML, "insert") match {
       case Success(matched,_) => println(matched)
       case Failure(msg,_) => println("FAILURE: " + msg)
       case Error(msg,_) => println("ERROR: " + msg)
