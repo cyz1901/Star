d40188c5cd909a7294adcae8c1edadb4d3d77b4e
cyz1901 <1903491992@qq.com>
2020-11-08 23:28:39 +0800

    fix:sql解析器

--- src/main/scala/tree/BPlusTree.scala
+++ src/main/scala/tree/BPlusTree.scala
@@ -12,10 +12,7 @@ import scala.collection.mutable.ArrayBuffer
 5.每个节点中的元素从小到大排列，节点当中k-1个元素正好是k个孩子包含的元素的值域分划*/
 
 
-class BPlusTree[A](
-
-                       ) {
-  //var root: RootNode[T] = RootNode[T](null,None)
+class BPlusTree[A]() {
   var h: Int = 0 //树的深度
   var tree_index = 0
   var index = 0
@@ -25,31 +22,70 @@ class BPlusTree[A](
   var root :Option[Node] = None
 
 
+  {
+    root = Some(Node(array = ArrayBuffer(),None)) /* 初始化根节点 */
+  }
 
 
-  def addData(data: A): Unit ={
-    root match {
+
+
+  def add(data: A): Unit ={
+    root.get.next match {
       case None =>
-        root = Some(Node(array = ArrayBuffer(),None))
-        root.get.array.addOne(Entry[A](index,data))
-        index += 1
-      case Some(x) =>
-        x.array.length match {
+        root.get.array.length match {
           case n if n >= M/2+1 =>
             root.get.array.addOne(Entry[A](index,data))
             var left = root.get.array.slice(0,M/2+1)
             var right = root.get.array.slice(M/2+1,root.get.array.length)
             var right_node = Node(array = right,None)
             var left_node = Node(array = left,Some(right_node))
-/*            root = Option(Node())*/
-            root.get.array = ArrayBuffer(Entry(0,left_node),Entry(1,right_node))
+            root.get.array = ArrayBuffer(Entry(left_node.array(0).key,left_node),Entry(right_node.array(0).key,right_node))
             root.get.next = Some(left_node)
           case n if n < M/2+1 =>
             root.get.array.addOne(Entry[A](index,data))
             index += 1
         }
+      case Some(x) =>
+        var leaf = binarySearch(root,index).array.addOne(Entry[A](index,data))
+        println(leaf)
     }
+//    root match {
+//      case None =>
+//        root = Some(Node(array = ArrayBuffer(),None))
+//        root.get.array.addOne(Entry[A](index,data))
+//        index += 1
+//      case Some(x) =>
+//        root.get.next match {
+//          case None =>
+//
+//          case Some(x) =>
+//            var leaf = binarySearch(root,index)
+//            leaf.array.length match {
+//              case n if n >= M/2+1 =>
+//                root.get.array.addOne(Entry[A](index,data))
+//                var left = root.get.array.slice(0,M/2+1)
+//                var right = root.get.array.slice(M/2+1,root.get.array.length)
+//                var right_node = Node(array = right,None)
+//                var left_node = Node(array = left,Some(right_node))
+//                root.get.array = ArrayBuffer(Entry(left_node.array(0).key,left_node),Entry(right_node.array(0).key,right_node))
+//                root.get.next = Some(left_node)
+//              case n if n < M/2+1 =>
+//                root.get.array.addOne(Entry[A](index,data))
+//                index += 1
+//            }
+//        }
+//    }
     }
+
+  def delete()= {
+
+  }
+
+  def update()   ={
+
+  }
+
+
 
   def binarySearch(root: Option[Node], i: Int): Node ={
     if(root.get.array(root.get.array.length/2).key > i) {
@@ -61,9 +97,26 @@ class BPlusTree[A](
       binarySearch(root, i)
     }
     else {
-      root.get.array(0).value.asInstanceOf[Node]
+      root.get.array(1).value.asInstanceOf[Node]
     }
   }
+
+//  def check_root(node: Node) = {
+//    node.array.length match {
+//      case n if n >= M/2+1 =>
+//        root.get.array.addOne(Entry[A](index,data))
+//        var left = root.get.array.slice(0,M/2+1)
+//        var right = root.get.array.slice(M/2+1,root.get.array.length)
+//        var right_node = Node(array = right,None)
+//        var left_node = Node(array = left,Some(right_node))
+//        /*            root = Option(Node())*/
+//        root.get.array = ArrayBuffer(Entry(left_node.array(0).key,left_node),Entry(right_node.array(0).key,right_node))
+//        root.get.next = Some(left_node)
+//      case n if n < M/2+1 =>
+//        root.get.array.addOne(Entry[A](index,data))
+//        index += 1
+//    }
+//  }
 
 
   }
@@ -104,11 +157,11 @@ object BPlusTree{
 object test{
   def main(args: Array[String]): Unit = {
     val a = new BPlusTree[Int]
-    a.addData(1)
-    a.addData(2)
-    a.addData(3)
-    a.addData(4)
-
-    println(a.root.get)
+    a.add(1)
+    a.add(2)
+    a.add(3)
+    a.add(4)
+    a.add(5)
+    println(a.root)
   }
 }
